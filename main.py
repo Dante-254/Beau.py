5776f465da5fa8ceced4096068b0dce3f3a33f46
Dante-254 <giciadaniel575@gmail.com>
2024-08-20 20:38:48 +0300

    $$commit

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 def hello(name, age, value):
   print('Hello '+name+' you are ' +str(age)+' of value '+str(value))
-  name = 'Rob'
   value = 28
   print('Hey '+name+' '+str(value))
 
-hello('Manoti', 22 , 44) 
+  print(age.bit_length())
+  print(value.bit_length())
+
+  views={
+    'id':'1',
+    'name':'Dante',
+    'date of birth':'22-feb-02'
+  }
+
+  
+
+  return name, id(name), id(age), id(views)
+
+print(hello('Manoti', 22 , 44))
