0d29c81a10f7250c8c0cc370f7425a034b8e89b5
Dante-254 <giciadaniel575@gmail.com>
2024-08-20 18:16:51 +0300

    def_py

---
+++ main.py
@@ -0,0 +1,7 @@
+def hello(name, age, value):
+  print('Hello '+name+' you are ' +str(age)+' of value '+str(value))
+  name = 'Rob'
+  value = 28
+  print('Hey '+name+' '+str(value))
+
+hello('Manoti', 22 , 44) 
