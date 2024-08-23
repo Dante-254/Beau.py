abfaf016ef2c6a4688495be44fe9e4d0399556a4
Dante-254 <giciadaniel575@gmail.com>
2024-08-23 12:36:13 +0300

    #2commit

--- global_fun.py
+++ global_fun.py
@@ -1,26 +1,25 @@
+from functools import reduce
 numbers = [1, 2, 3]
 
 # def double(a):
 #     return a*2
-
 # double = lambda a : a*2
 
 result = map(lambda a : a*2, numbers)
 
 print(list(result))
 
-list_tuples = [1, 2, 3, 4]
 expenses = [
     ('Dave', 20),
     ('Rob', 30)
 ]
-sum=0
-for expense in expenses:
-    print(expense)
-    print(expense[1])
-    sum += expense[1]
-    print(sum)
-
-print(expenses[1])
-print(list_tuples[1])
+# sum=0
+# for expense in expenses:
+#     print(expense)
+#     print(expense[1])
+#     sum += expense[1]
+#     print(sum)
+sum = reduce(lambda a, b: a[1] + b[1], expenses)
 print(sum)
+
+
