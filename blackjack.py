8369e105649b658ebbbf86307260c7baf38c92df
Dante-254 <giciadaniel575@gmail.com>
2024-09-05 15:23:37 +0300

    with the if statement to assign value to card rank

--- blackjack.py
+++ blackjack.py
@@ -6,15 +6,32 @@ ranks = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
  
 for suit in suits:
     for rank in ranks:
-        cards.append([rank, suit])
+        cards.append([suit, rank])
 
 def shuffle():
     random.shuffle(cards)
 
-def deal():
-    card = cards.pop()
-    return card
+def deal(number):
+    cards_delt = [] 
+    for x in range(number):
+        card = cards.pop()
+        cards_delt.append(card)
+    return cards_delt
 
-shuffle() 
-cards_delt = deal()
-print(cards_delt)
+shuffle()
+
+cards_delt = deal(2)
+card = cards_delt[0]
+rank = card[1]
+if rank == 'A':
+    value = 11
+elif rank == '10' or rank == 'J' or rank == 'Q' or rank == 'K':
+    value = 10
+else:
+    value = rank
+rank_dict = {
+    'rank':rank, 'value':value
+}
+print(card)
+print(rank_dict['rank'])
+print(rank_dict['value'])
